911494f6a3f699ecc239148758181cc22e6ac922
Chinoms <chinoms925@gmail.com>
2020-08-28 11:51:26 -0700

    Update dashboard.blade.php

--- resources/views/partials/dashboard.blade.php
+++ resources/views/partials/dashboard.blade.php
@@ -83,7 +83,7 @@
                         <div class="card">
                             <div class="card-header">
                                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                         <span class="navbar-toggler-icon"></span>
                                     </button>
 
